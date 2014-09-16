abbdf02872a002ff755662d25541e0681c143c37
Roman Belkov <romanbelkov@gmail.com>
2014-09-16 15:15:41 +0400

    Update LCM.hs

--- LCM.hs
+++ LCM.hs
@@ -1,7 +1,7 @@
 gcd' a b | a == b = a
-		 | a < b = gcd' b a
-	 	 | a > b =
-		 	if r == 0 then b else b `gcd'` r
-			where r = a `mod` b
+	 | a < b = gcd' b a
+	 | a > b =
+		if r == 0 then b else b `gcd'` r
+		where r = a `mod` b
 
 lcm' x y =  (x * y) `div` (x `gcd'` y)
