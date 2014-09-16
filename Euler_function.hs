8dfe1eb22aa1cdb25f720ad33a954862e58fdc43
Roman Belkov <romanbelkov@gmail.com>
2014-09-16 15:49:37 +0400

    Added new task

---
+++ Euler_function.hs
@@ -0,0 +1,11 @@
+gcd' a b | a == b = a
+	     | a < b = gcd' b a
+	     | a > b =
+	     	if r == 0 then b else b `gcd'` r
+			where r = a `mod` b
+
+phi 1 = 1
+phi x = loop 2 1 x 
+	where loop a acc x | x == a = acc
+                       | x `gcd'` a == 1 = loop (a + 1) (acc + 1) x
+                       | otherwise = loop (a + 1) acc x
