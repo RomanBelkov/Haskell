5476f45aa6fe10e23bf6d9c0be9103abb9ab7ea2
Roman Belkov <romanbelkov@gmail.com>
2014-09-16 15:17:09 +0400

    Update The_divisor_function.hs

--- The_divisor_function.hs
+++ The_divisor_function.hs
@@ -1,5 +1,5 @@
 dc x = dc' 2 2 x
 	where dc' a acc x | a * a > x = acc
-				      | a * a == x = (acc + 1)
-				      | x `mod` a == 0 = dc' (a + 1) (acc + 2) x
-				      | otherwise = dc' (a + 1) acc x
+			  | a * a == x = (acc + 1)
+			  | x `mod` a == 0 = dc' (a + 1) (acc + 2) x
+			  | otherwise = dc' (a + 1) acc x
